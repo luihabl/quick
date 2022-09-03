b4ad73ca5debafdbdf717ec711cbadac6ec1761f
Lui Habl <txaicalvoso@gmail.com>
2022-09-03 14:37:55 +0200

    Global macro definitions

--- include/quick.h
+++ include/quick.h
@@ -7,21 +7,21 @@
 
 #include <GLFW/glfw3.h>
 
-#ifdef _WIN32
-#undef APIENTRY
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
-#endif
-#ifdef __APPLE__
-#define GLFW_EXPOSE_NATIVE_COCOA
-#include <GLFW/glfw3native.h>   // for glfwGetCocoaWindow()
-#endif
+    #ifdef _WIN32
+    #undef APIENTRY
+    #define GLFW_EXPOSE_NATIVE_WIN32
+    #include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
+    #endif
+    #ifdef __APPLE__
+    #define GLFW_EXPOSE_NATIVE_COCOA
+    #include <GLFW/glfw3native.h>   // for glfwGetCocoaWindow()
+    #endif
 
 #endif // QUICK_PLATFORM_GLFW
 
 #ifdef QUICK_PLATFORM_SDL2
 
-#include <SDL.h>
+    #include <SDL.h>
 
 #endif //QUICK_PLATFORM_SDL2
 
